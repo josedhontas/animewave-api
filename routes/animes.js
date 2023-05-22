93b1d0dac067904aa994a65975e540f662fa0f4a
josedhonatas <dhonatas@academico.ufs.br>
2023-05-22 15:23:20 -0300

    mudancinha

--- routes/animes.js
+++ routes/animes.js
@@ -4,40 +4,62 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/', (req, res) => {
-  
-    // Lê os usuários existentes do arquivo JSON
-    let animes = [];
-    try {
-      const data = fs.readFileSync('animes.json');
-      animes = JSON.parse(data);
-    } catch (error) {
-      console.error(error);
-    }
-  
-  
-    res.send(animes);
+  let animes = [];
+  try {
+    const data = fs.readFileSync('animes.json');
+    animes = JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
+
+  const formattedAnimes = animes.map(anime => {
+    return {
+      id: anime.id,
+      titulo: anime.titulo,
+      descricao: anime.descricao,
+      urlImagem: anime.urlImagem,
+    };
   });
 
+  res.send(formattedAnimes);
+});
 
-  router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-  
-    let animes = [];
-    try {
-      const data = fs.readFileSync('animes.json');
-      animes = JSON.parse(data);
-    } catch (error) {
-      console.error(error);
-    }
-  
-    // Busca o usuário pelo CPF na lista de usuários
-    const anime = animes.find(u => u.id === id);
-  
-    if (!anime) {
-      return res.status(404).send('Anime não encontrado');
-    }
-  
-    res.send(anime);
-  });
+router.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  let animes = [];
+  try {
+    const data = fs.readFileSync('animes.json');
+    animes = JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
+
+  const anime = animes.find(u => u.id === id);
+
+  if (!anime) {
+    return res.status(404).send('Anime não encontrado');
+  }
+
+  const formattedAnime = {
+    id: anime.id,
+    titulo: anime.titulo,
+    descricao: anime.descricao,
+    urlImagem: anime.urlImagem,
+    quantidadeEpisodios: anime.quantidadeEpisodios,
+    urlEpisodio: anime.urlEpisodio,
+    episodios: [],
+  };
+
+  for (let i = 1; i <= anime.quantidadeEpisodios; i++) {
+    const episodio = {
+      numero: i,
+      link: `${anime.urlEpisodio}${i}.mp4`,
+    };
+    formattedAnime.episodios.push(episodio);
+  }
+
+  res.send(formattedAnime);
+});
 
 module.exports = router;
