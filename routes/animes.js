8e133af3c2cf72e5a9cc48f239570c85ea8a47c8
josedhonatas <dhonatas@academico.ufs.br>
2023-05-25 18:48:18 -0300

    changeda

--- routes/animes.js
+++ routes/animes.js
@@ -64,83 +64,6 @@ router.get('/', (req, res) => {
   res.send(formattedAnimes);
 });
 
-/**
- * @swagger
- * /animes/{id}:
- *   get:
- *     summary: Retorna um anime pelo ID
- *     tags:
- *       - Animes
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *           description: ID do anime
- *     responses:
- *       200:
- *         description: Anime retornado com sucesso
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Anime'
- *       404:
- *         description: Anime não encontrado
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- */
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     Anime:
- *       type: object
- *       properties:
- *         id:
- *           type: integer
- *           description: ID do anime
- *         titulo:
- *           type: string
- *           description: Título do anime
- *         descricao:
- *           type: string
- *           description: Descrição do anime
- *         urlImagem:
- *           type: string
- *           description: URL da imagem do anime
- *         quantidadeEpisodios:
- *           type: integer
- *           description: Quantidade de episódios do anime
- *         urlEpisodio:
- *           type: string
- *           description: URL base dos episódios do anime
- *         episodios:
- *           type: array
- *           items:
- *             $ref: '#/components/schemas/Episodio'
- */
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     Episodio:
- *       type: object
- *       properties:
- *         numero:
- *           type: integer
- *           description: Número do episódio
- *         link:
- *           type: string
- *           description: URL do episódio
- *       example:
- *         numero: 1
- *         link: https://lightspeedst.net/s3/mp4/death-note/sd/1.mp4
- */
 
 
 router.get('/:id', (req, res) => {
@@ -167,16 +90,8 @@ router.get('/:id', (req, res) => {
     urlImagem: anime.urlImagem,
     quantidadeEpisodios: anime.quantidadeEpisodios,
     urlEpisodio: anime.urlEpisodio,
-    episodios: [],
+    episodios: anime.episodios,
   };
-
-  for (let i = 1; i <= anime.quantidadeEpisodios; i++) {
-    const episodio = {
-      numero: i,
-      link: `${anime.urlEpisodio}${i}.mp4`,
-    };
-    formattedAnime.episodios.push(episodio);
-  }
 
   res.send(formattedAnime);
 });
